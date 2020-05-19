7c672897aab873f93e0bb412f67ebcfecdf35bd1
yjjung@mac <yjjung@tecace.com>
2020-05-20 00:19:23 +0900

    아파트 이름, 행정구역 코드 표시 ui

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -2,8 +2,9 @@
   <div class="login">
     <div class="header">로고</div>
     <div class="body">
-      <input id="addressInput" placeholder="살고계신 아파트 이름을 검색해 주세요." @click="getAddress"/>
+      <input readonly v-model="address" id="addressInput" placeholder="살고계신 아파트 이름을 검색해 주세요." @click="getAddress"/>
       <div id="loginBtn" @click="$router.push('main')">인증</div>
+      <div>행정구역코드: {{bcode}}</div>
     </div>
   </div>
 </template>
@@ -17,7 +18,8 @@ export default {
   },
   data () {
     return {
-    
+      address:'',
+      bcode:'',
     }
   },
   methods:{
@@ -25,18 +27,23 @@ export default {
       let recaptchaScript = document.createElement('script')
       recaptchaScript.setAttribute('src', 'https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js')
       document.head.appendChild(recaptchaScript)
-      setTimeout(function(){
-
       
-      new global.daum.Postcode({
-          oncomplete: function(data) {
-            console.log(data)
-              // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분입니다.
-              // 예제를 참고하여 다양한 활용법을 확인해 보세요.
-          }
-      }).open();
-
-      },500)
+      
+      let that = this
+      let interval = null
+      interval = setInterval(function(){
+        if(!global.daum.Postcode) return
+        new global.daum.Postcode({
+            oncomplete: function(data) {
+              console.log(data)
+              that.address = data.buildingName
+              that.bcode = data.bcode
+                // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분입니다.
+                // 예제를 참고하여 다양한 활용법을 확인해 보세요.
+            }
+        }).open();
+        clearInterval(interval)
+      },50)
     },
   },
   mounted(){
@@ -60,11 +67,11 @@ export default {
 }
 #addressInput{
   padding:0 4vw;
-  font-size:4vw;
+  font-size:4.5vw;
   height:12vw;
   width:100%;
   background:#f5f5f5;
-  color:#aaa;
+  color:black;
 }
 #loginBtn{
   margin-top:4vw;
