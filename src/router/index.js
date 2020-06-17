ae338ee220f52bf876d20b043e7b2d82d73cc8ae
unknown <cyyworld71@gmail.com>
2020-06-17 15:40:38 +0900

    add user, caht, alarm, search page

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,12 @@ import Router from 'vue-router'
 import Main from '@/pages/main'
 import Login from '@/pages/login'
 import Auth from '@/pages/auth'
-import PostDetail from '@/pages/postDetail'
 import PostWriting from '@/pages/postWriting'
+import PostDetail from '@/pages/postDetail'
+import Search from '@/pages/search'
+import Chat from '@/pages/chat'
+import Alarm from '@/pages/alarm'
+import User from '@/pages/user'
 Vue.use(Router)
 
 export default new Router({
@@ -39,6 +43,26 @@ export default new Router({
       path: '/auth',
       name: 'auth',
       component: Auth
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: Search
+    },
+    {
+      path: '/chat',
+      name: 'chat',
+      component: Chat
+    },
+    {
+      path: '/alarm',
+      name: 'alarm',
+      component: Alarm
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: User
     }
   ]
 })
