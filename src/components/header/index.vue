567e9273f95f61ebcf508d751bf1a0a001fa842f
yjjung@mac <yjjung@tecace.com>
2020-08-28 17:57:29 +0900

    return bug

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -31,9 +31,8 @@ export default {
       let width =  $("#"+this.currentTopicId).width()
       if(!this.$refs["vs"] || !offset || !offset.left) return
       let elCenter = offset.left + width/2
-      let targetScrollLeft = elCenter - this.$refs["vs"].scroller.__clientWidth/2+ this.$refs["vs"].scroller.__clientWidth*0.04
-      console.log(targetScrollLeft)
-      if(targetScrollLeft<0) targetScrollLeft = 0
+      let targetScrollLeft = elCenter - this.$refs["vs"].scroller.__clientWidth/2
+      if(targetScrollLeft<0) targetScrollLeft = 1
       this.$refs["vs"].scrollTo(
         {x:targetScrollLeft}, 300, "easeInQuad"
       )
